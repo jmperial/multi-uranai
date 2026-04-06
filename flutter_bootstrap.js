0972f1374a261d9ab180ca99724d79dc04a9397e
jmperial <jmperial@users.noreply.github.com>
2026-04-07 02:19:54 +0900

    Fix: viewport meta for mobile, rename to 占いダッシュボード, fix CanvasKit local loading

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -36,10 +36,7 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 _flutter.loader.load({
-  config: {
-    canvasKitBaseUrl: "canvaskit/",
-  },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2601571608" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "638098668" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
