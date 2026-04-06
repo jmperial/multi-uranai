e6193c99f58e8aff33f51671ebf60edaf952db99
jmperial <jmperial@users.noreply.github.com>
2026-04-07 02:32:34 +0900

    Dashboard: ゲージメーター風カード、パフォーマンス改善

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3644679477" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1887475571" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
